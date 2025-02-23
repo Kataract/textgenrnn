e0a9f6c4eeca16ab788538bdf45331746b613af2
Zach Passen <z@passen.dev>
2025-02-23 14:41:09 -0700

    remove some more of these

--- textgenrnn/textgenrnn.py
+++ textgenrnn/textgenrnn.py
@@ -235,7 +235,7 @@ class textgenrnn:
             if new_model:
                 weights_path = None
             else:
-                weights_path = "{}_weights.hdf5".format(self.config['name'])
+                weights_path = "{}_weights.h5".format(self.config['name'])
                 self.save(weights_path)
 
 
@@ -260,7 +260,7 @@ class textgenrnn:
                 if new_model:
                     weights_path = None
                 else:
-                    weights_path = "{}_weights.hdf5".format(self.config['name'])
+                    weights_path = "{}_weights.h5".format(self.config['name'])
 
                 strategy = distribute.MirroredStrategy()
                 with strategy.scope():
@@ -357,7 +357,7 @@ class textgenrnn:
                             multi_gpu=multi_gpu,
                             **kwargs)
 
-    def save(self, weights_path="textgenrnn_weights_saved.hdf5"):
+    def save(self, weights_path="textgenrnn_weights_saved.h5"):
         self.model.save_weights(weights_path)
 
     def load(self, weights_path):
